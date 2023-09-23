3b679ca33cc164794b3dbeb54af571a0785ed40a
Owner <bill.colak@gmail.com>
2023-09-23 13:17:42 -0600

    Added logo

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -3,15 +3,17 @@ import TeamSwitcher from "@/components/team-switcher";
 import {MainNav} from "@/components/main-nav";
 import {Search} from "@/components/search";
 import {UserNav} from "@/components/user-nav";
+import Image from "next/image";
 
 function Navigation() {
     return (
         <div className="border-b">
             <div className="flex h-16 items-center px-4">
-                <TeamSwitcher />
+                <Image src={'/logo.png'} alt={'icon'} width={100} height={60} className={'mr-2'}/>
                 <MainNav className="mx-6" />
                 <div className="ml-auto flex items-center space-x-4">
                     <Search />
+                    <TeamSwitcher />
                     <UserNav />
                 </div>
             </div>
