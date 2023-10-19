d8111e6dbafb3230500ccfdeaa4a29820264c10e
Owner <bill.colak@gmail.com>
2023-10-19 11:23:15 -0600

    minor changes to demo table.

--- src/app/(pages)/supabasedemo/page.tsx
+++ src/app/(pages)/supabasedemo/page.tsx
@@ -38,35 +38,36 @@ export default function TableDemo() {
             <TableCaption>{loading ? 'Loading...': 'A list of Car Sales.'}</TableCaption>
             <TableHeader>
                 <TableRow>
-                    <TableHead className="w-[100px]">ActualCashValue</TableHead>
-                    <TableHead>CustomerID</TableHead>
-                    <TableHead>EmployeeID</TableHead>
-                    <TableHead>FinancingID</TableHead>
-                    <TableHead>FinandInsurance</TableHead>
-                    <TableHead>GrossProfit</TableHead>
-                    <TableHead>Holdback</TableHead>
-                    <TableHead>id</TableHead>
+                    <TableHead className="w-[100px]">Id</TableHead>
                     <TableHead>StockNumber</TableHead>
+                    <TableHead>VehicleMake</TableHead>
+                    <TableHead>ActualCashValue</TableHead>
+                    <TableHead>GrossProfit</TableHead>
+                    <TableHead>FinandInsurance</TableHead>
+                    <TableHead>Holdback</TableHead>
                     <TableHead>Total</TableHead>
+                    <TableHead>EmployeeID</TableHead>
                     <TableHead>TradeInId</TableHead>
-                    <TableHead className="text-right">VechileMake</TableHead>
+                    <TableHead>CustomerID</TableHead>
+                    <TableHead>FinancingID</TableHead>
+                    <TableHead className="text-right">TradeInID</TableHead>
                 </TableRow>
             </TableHeader>
             <TableBody>
                 {sales && sales.map((sale) => (
                     <TableRow key={sale.id}>
-                        <TableCell className="font-medium">{sale.ActualCashValue}</TableCell>
-                        <TableCell>{sale.CustomerID}</TableCell>
-                        <TableCell>{sale.EmployeeID}</TableCell>
-                        <TableCell>{sale.FinancingID}</TableCell>
-                        <TableCell>{sale.FinAndInsurance}</TableCell>
-                        <TableCell>{sale.GrossProfit}</TableCell>
-                        <TableCell>{sale.Holdback}</TableCell>
-                        <TableCell>{sale.id}</TableCell>
+                        <TableCell className="font-medium">{sale.id}</TableCell>
                         <TableCell>{sale.StockNumber}</TableCell>
+                        <TableCell>{sale.VehicleMake}</TableCell>
+                        <TableCell>{sale.ActualCashValue}</TableCell>
+                        <TableCell>{sale.GrossProfit}</TableCell>
+                        <TableCell>{sale.FinAndInsurance}</TableCell>
+                        <TableCell>{sale.Holdback}</TableCell>
                         <TableCell>{sale.Total}</TableCell>
-                        <TableCell>{sale.TradeInID}</TableCell>
-                        <TableCell className="text-right">{sale.VehicleMake}</TableCell>
+                        <TableCell>{sale.EmployeeID}</TableCell>
+                        <TableCell>{sale.CustomerID}</TableCell>
+                        <TableCell>{sale.FinancingID}</TableCell>
+                        <TableCell className="text-right">{sale.TradeInID}</TableCell>
                     </TableRow>
                 ))}
             </TableBody>
