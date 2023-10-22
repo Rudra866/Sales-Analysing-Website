1bcdfc965c65ef035b4958181f840a4299488055
rudrapatel <cvb652@usask.ca>
2023-10-22 14:07:56 -0600

    Merge bill with my branch with database example

--- src/app/(pages)/dashboard/page.tsx
+++ src/app/(pages)/dashboard/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { Metadata } from "next"
 import Image from "next/image"
 
@@ -16,17 +17,65 @@ import {
   TabsTrigger,
 } from "@/registry/new-york/ui/tabs"
 import { CalendarDateRangePicker } from "./components/date-range-picker"
+import { MainNav } from "./components/main-nav"
 import { Overview } from "./components/overview"
 import { RecentSales } from "./components/recent-sales"
+import { Search } from "./components/search"
+import TeamSwitcher from "./components/team-switcher"
+import { UserNav } from "./components/user-nav"
+import {useEffect, useState} from "react";
+import {Tables} from "@/lib/database.types";
+import supabase from "@/lib/supabase";
+import {DbResult} from "@/lib/types";
 
 
 
 export default function DashboardPage() {
+  const [sales, setSales] = useState<Tables<'Sales'>[]>();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchTable();
+  }, []);
+
+  const fetchTable = async () => {
+    try {
+      setLoading(true)
+
+      let { data: Sales, error } = await supabase
+          .from('Sales')
+          .select('*')
+          .order('id', { ascending: true })
+          .limit(30)
+
+      if (error) throw error
+      if (Sales) {
+        setSales( Sales as DbResult<typeof Sales[]>)
+        console.log(Sales)
+      }
+
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setLoading(false)
+    }
+  }
   return (
     <>
       <div className="md:hidden">
+
       </div>
       <div className="hidden flex-col md:flex">
+        {/*<div className="border-b">*/}
+        {/*  <div className="flex h-16 items-center px-4">*/}
+        {/*    <TeamSwitcher />*/}
+        {/*    <MainNav className="mx-6" />*/}
+        {/*    <div className="ml-auto flex items-center space-x-4">*/}
+        {/*      <Search />*/}
+        {/*      <UserNav />*/}
+        {/*    </div>*/}
+        {/*  </div>*/}
+        {/*</div>*/}
         <div className="flex-1 space-y-4 p-8 pt-6">
           <div className="flex items-center justify-between space-y-2">
             <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
