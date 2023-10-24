8b44da391bac23a13d9cc7edb1bd9a4cad017b08
rudrapatel <cvb652@usask.ca>
2023-10-24 11:31:53 -0600

    Extracted database from fetch with the selected date range from calender, and tried to plot on graph but error

--- src/app/(pages)/dashboard/components/overview.tsx
+++ src/app/(pages)/dashboard/components/overview.tsx
@@ -1,78 +1,49 @@
-"use client"
+import { Line, LineChart, ResponsiveContainer, XAxis, YAxis } from 'recharts';
 
-import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from "recharts"
 
-const data = [
-  {
-    name: "Jan",
-    total: Math.floor(Math.random() * 5000) + 1000,
-  },
-  {
-    name: "Feb",
-    total: Math.floor(Math.random() * 5000) + 1000,
-  },
-  {
-    name: "Mar",
-    total: Math.floor(Math.random() * 5000) + 1000,
-  },
-  {
-    name: "Apr",
-    total: Math.floor(Math.random() * 5000) + 1000,
-  },
-  {
-    name: "May",
-    total: Math.floor(Math.random() * 5000) + 1000,
-  },
-  {
-    name: "Jun",
-    total: Math.floor(Math.random() * 5000) + 1000,
-  },
-  {
-    name: "Jul",
-    total: Math.floor(Math.random() * 5000) + 1000,
-  },
-  {
-    name: "Aug",
-    total: Math.floor(Math.random() * 5000) + 1000,
-  },
-  {
-    name: "Sep",
-    total: Math.floor(Math.random() * 5000) + 1000,
-  },
-  {
-    name: "Oct",
-    total: Math.floor(Math.random() * 5000) + 1000,
-  },
-  {
-    name: "Nov",
-    total: Math.floor(Math.random() * 5000) + 1000,
-  },
-  {
-    name: "Dec",
-    total: Math.floor(Math.random() * 5000) + 1000,
-  },
-]
+interface OverviewProps {
+    d: { GrossProfit: number; SaleTime: string | null }[];
+}
 
-export function Overview() {
-  return (
-    <ResponsiveContainer width="100%" height={350}>
-      <BarChart data={data}>
-        <XAxis
-          dataKey="name"
-          stroke="#888888"
-          fontSize={12}
-          tickLine={false}
-          axisLine={false}
-        />
-        <YAxis
-          stroke="#888888"
-          fontSize={12}
-          tickLine={false}
-          axisLine={false}
-          tickFormatter={(value) => `$${value}`}
-        />
-        <Bar dataKey="total" fill="#adfa1d" radius={[4, 4, 0, 0]} />
-      </BarChart>
-    </ResponsiveContainer>
-  )
+
+
+// Overview component
+export function Overview({ d }: OverviewProps ) {
+
+    if (!Array.isArray(d)) {
+        // If d is not an array, make it an array
+        d=[d];
+    }
+    // Group sales data by day
+    const groupedData: { [key: string]: number } = {};
+    d.forEach(item => {
+        const date = item.SaleTime ? new Date(item.SaleTime).toISOString().split('T')[0] : 'Unknown';
+        groupedData[date] = (groupedData[date] || 0) + item.GrossProfit;
+    });
+    const data = Object.keys(groupedData).map(date => ({
+        name: date,
+        GrossProfit: groupedData[date],
+    }));
+
+    return (
+        <ResponsiveContainer width="100%" height={350}>
+            <LineChart data={data}>
+                <XAxis
+                    dataKey="name"
+                    stroke="#888888"
+                    fontSize={12}
+                    tickLine={false}
+                    axisLine={false}
+                />
+                <YAxis
+                    stroke="#888888"
+                    fontSize={12}
+                    tickLine={false}
+                    axisLine={false}
+                    tickFormatter={(value) => `$${value}`}
+                />
+                <Line type="monotone" dataKey="GrossProfit" stroke="#adfa1d" strokeWidth={2}/>
+            </LineChart>
+        </ResponsiveContainer>
+    );
 }
