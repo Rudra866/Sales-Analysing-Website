6845bed082645f045e2b04fe8350882729e3e7a9
Ryan Schaffer (rys686) <rys686@mail.usask.ca>
2023-10-27 19:29:09 -0600

    documentation updates to use-auth hook.

--- src/hooks/use-auth.ts
+++ src/hooks/use-auth.ts
@@ -2,17 +2,25 @@
 import {useContext} from 'react'
 import {AuthContext, AuthContextType} from "@/components/auth-provider";
 
-
-
-
-// export the useAuth hook, use this to access employee/role variables and restrict ability on low security features
-// for high security routes use the middleware options or use server component/server action
-export const useAuth = (): AuthContextType => {
+/**
+ * Hook to access user/employee/role variables and authentication functions.
+ * This can be used for conditional rendering to personalize the website for the employee, or block rendering
+ * buttons that are unusable (but still protected). This CANNOT be used solely for securing routes or actions that could
+ * easily be bypassed, use middleware or server routes instead for these actions.
+ * If there is no session currently, `user`, `employee` and `role` will all be null.
+ *  @returns {AuthContextType}
+ *  An object containing the following properties:
+ *  - `user`: The user authentication object.
+ *  - `employee`: The employee object.
+ *  - `role`: The role object of the employee.
+ *  - `signIn`: Function for signing in.
+ *  - `signOut`: Function for signing out.
+ *  - `signUp`: Function for signing up.
+ */
+export default function useAuth(): AuthContextType {
   const auth = useContext(AuthContext);
   if (auth === null) {
     throw new Error('useAuth must be used within an AuthProvider');
   }
   return auth;
 };
-
-export default useAuth;
