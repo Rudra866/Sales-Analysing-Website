3f756813d72c060b76f696f318fac40c248a95fd
Ryan Schaffer (rys686) <rys686@mail.usask.ca>
2023-11-04 07:30:59 -0600

    added an employee avatar to go along with avatar db entry

--- src/app/(pages)/dashboard/components/recent-sales.tsx
+++ src/app/(pages)/dashboard/components/recent-sales.tsx
@@ -1,12 +1,16 @@
 import {Avatar, AvatarFallback, AvatarImage,} from "@/components/ui/avatar"
 import {useRouter} from "next/navigation";
 import {useDashboard} from "@/app/(pages)/dashboard/components/dashboard-provider";
+import EmployeeAvatar from "@/components/dashboard-components/EmployeeAvatar";
+import useAuth from "@/hooks/use-auth";
 /** @ignore these for now */
 export function RecentSales() {
     const {date, setDate} = useDashboard()
+    const {employee} = useAuth()
 
+    /* can we have this take an employee? */
     function SalesRow({name, email, amount, id}: { name: string; email: string; amount: number; id: string }) {
-        const random = (max: number) => Math.floor(Math.random() * max) + 1
+        // const random = (max: number) => Math.floor(Math.random() * max) + 1
         const router = useRouter()
 
         return (
@@ -15,10 +19,11 @@ export function RecentSales() {
                      router.push(`/profile/${id}`)
                  }}
             >
-                <Avatar className="h-9 w-9">
-                    <AvatarImage src={`/avatars/0${random(5)}.png`} alt="Avatar"/>
-                    <AvatarFallback>{name[0]}</AvatarFallback>
-                </Avatar>
+                {/*<Avatar className="h-9 w-9">*/}
+                {/*    <AvatarImage src={`/avatars/0${random(5)}.png`} alt="Avatar"/>*/}
+                {/*    <AvatarFallback>{name[0]}</AvatarFallback>*/}
+                {/*</Avatar>*/}
+                <EmployeeAvatar employee={employee!}/> {/* for now since no user info yet, just use signed in employee */}
                 <div className="ml-4 space-y-1">
                     <p className="text-sm font-medium leading-none">{name}</p>
                     <p className="text-sm text-muted-foreground">{email}</p>
---
+++ src/components/dashboard-components/EmployeeAvatar.tsx
@@ -0,0 +1,12 @@
+import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
+import {Employee} from "@/lib/database";
+
+
+export default function EmployeeAvatar({employee}: { employee: Employee }) {
+  return (
+      <Avatar className="h-9 w-9">
+        <AvatarImage src={`/avatars/${employee.Avatar}`} alt="Avatar"/>
+        <AvatarFallback>{employee.Name[0]}</AvatarFallback>
+      </Avatar>
+  );
+}
