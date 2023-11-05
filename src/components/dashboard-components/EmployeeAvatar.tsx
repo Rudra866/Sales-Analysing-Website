e5090c36ce633ef31100fade5911ccbde1c8baba
Ryan Schaffer (rys686) <rys686@mail.usask.ca>
2023-11-04 23:02:54 -0600

    fixed employee avatar not having a fallback.

--- src/components/dashboard-components/EmployeeAvatar.tsx
+++ src/components/dashboard-components/EmployeeAvatar.tsx
@@ -2,11 +2,11 @@ import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
 import {Employee} from "@/lib/database";
 
 
-export default function EmployeeAvatar({employee}: { employee: Employee }) {
+export default function EmployeeAvatar({employee}: { employee?: Employee }) {
   return (
       <Avatar className="h-9 w-9">
-        <AvatarImage src={`/avatars/${employee.Avatar}`} alt="Avatar"/>
-        <AvatarFallback>{employee.Name[0]}</AvatarFallback>
+        <AvatarImage src={`/avatars/${employee?.Avatar ?? "01.png"}`} alt="Avatar"/>
+        <AvatarFallback>{employee?.Name[0] ?? 'U'}</AvatarFallback>
       </Avatar>
   );
 }
