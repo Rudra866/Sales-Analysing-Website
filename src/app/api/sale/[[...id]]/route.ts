8d0911e4e2a60ede3bcb385c42d7feaf2b2398c3
Ryan Schaffer (rys686) <rys686@mail.usask.ca>
2023-11-16 22:01:08 -0600

    added api/sale POST route.

--- src/app/api/sale/[[...id]]/route.ts
+++ src/app/api/sale/[[...id]]/route.ts
@@ -2,7 +2,10 @@
 // right now, the RLS rules are pretty relaxed and anyone can read or edit any sale.
 // this route will handle all requests to /api/sale as well as /api/sale/{id}
 
-import {NextResponse} from "next/server";
+import {NextRequest, NextResponse} from "next/server";
+import {Database, getEmployeeFromAuthUser, getRoleFromEmployee, getSupabaseRouteHandlerClient} from "@/lib/database";
+import {createClient} from "@supabase/supabase-js";
+import {cookies} from "next/headers";
 
 // handle retrieving a single sale, or all the sales. Protects sales from being read by users with no permission.
 export function GET(request: Request) {
@@ -10,8 +13,26 @@ export function GET(request: Request) {
 }
 
 // handle new sale input to database
-export function POST(request: Request) {
-  return NextResponse.json({error: "Not yet implemented."}, {status: 405})
+export async function POST(request: NextRequest) {
+  const supabase = getSupabaseRouteHandlerClient(cookies());
+  const {data: {session}} = await supabase.auth.getSession();
+  const employee = await getEmployeeFromAuthUser(supabase, session!.user)
+  const role = await getRoleFromEmployee(supabase, employee);
+
+  // reject unauthorized users
+  if (!role.WritePermission) {
+    return NextResponse.json({error: "Forbidden"}, {status: 401});
+  }
+
+  // otherwise, setup admin client and create new sale.
+  const supabaseAdmin =
+      createClient<Database>(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.SUPABASE_SERVICE_KEY!)
+
+  // TODO need to make this return a better status, maybe returning the item as well.
+  const res = await supabaseAdmin.rpc("create_new_sale", {sale: await request.json()});
+  return new Response(null, {
+    status: res.status,
+  })
 }
 
 export function PATCH(request: Request) {
