0254af17bfe63f8a0fb84df3c472f456513c66f6
Ryan Schaffer (rys686) <rys686@mail.usask.ca>
2023-11-22 12:16:46 -0600

    fixed build/tests

--- src/tests/components/SalesTable.test.tsx
+++ src/tests/components/SalesTable.test.tsx
@@ -1,9 +1,10 @@
 import UserAuthForm from "@/app/authentication/components/user-auth-form";
-import {render, screen, waitFor} from '@testing-library/react';
-import {act} from "react-dom/test-utils";
-import {userEvent} from "@storybook/testing-library";
-import {getCookie, getCookieName, test_admin_info} from "../../../jest-setup";
-import SalesTable from "@/components/tables/SalesTable";
+import { render, screen, waitFor } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
+import { userEvent } from "@storybook/testing-library";
+import { getCookie, getCookieName, test_admin_info } from "../../../jest-setup";
+import SalesTable from "@/components/tables/sales-table";
+import { test_employee_set, test_sales_set } from "@/tests/test_data";
 
 describe("Sales Table Component", () => {
   // use this at the start of all tests to login as the integration admin with all permissions
@@ -11,9 +12,9 @@ describe("Sales Table Component", () => {
   beforeAll(async () => {
     render(<UserAuthForm />);
 
-    const emailField = screen.getByRole('textbox');
-    const passwordField = screen.getByPlaceholderText('password');
-    const submitButton = screen.getByRole('button', { name: 'Sign In' });
+    const emailField = screen.getByRole("textbox");
+    const passwordField = screen.getByPlaceholderText("password");
+    const submitButton = screen.getByRole("button", { name: "Sign In" });
 
     await act(async () => {
       await userEvent.type(emailField, test_admin_info.email);
@@ -22,20 +23,28 @@ describe("Sales Table Component", () => {
     });
 
     // Wait for the success indicator (or any other element that indicates successful login)
-    await waitFor(() => screen.getByTestId('success'), { timeout: 10000 });
+    await waitFor(() => screen.getByTestId("success"), { timeout: 10000 });
 
     // Assert that the authentication cookie is present
     const cookie = getCookie(getCookieName());
     expect(cookie).not.toBeNull();
   });
 
-  it('test rendering the add sale modal on click', async () => {
-    render(<SalesTable />);
-    const addSaleButton = screen.getByText('Add Sale');
+  it("test rendering the add sale modal on click", async () => {
+    render(
+      <SalesTable
+        data={test_sales_set}
+        employees={test_employee_set}
+        loading={false}
+      />,
+    );
+    const addSaleButton = screen.getByText("Add Sale");
     await act(async () => {
       await userEvent.click(addSaleButton);
     });
-    await waitFor(() => screen.getByPlaceholderText("Customer Name"), {timeout:10000})
+    await waitFor(() => screen.getByPlaceholderText("Customer Name"), {
+      timeout: 10000,
+    });
     expect(screen.getByPlaceholderText("Customer Name")).toBeInTheDocument();
-  })
-})
+  });
+});
