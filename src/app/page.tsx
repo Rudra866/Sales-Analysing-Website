d98a67e3b2f68ad3f662d6bf069a3643c03b76c2
Ryan Schaffer (rys686) <rys686@mail.usask.ca>
2023-11-22 13:24:10 -0600

    fixed root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
-'use client'
+"use client";
 import useAuth from "@/hooks/use-auth";
-import {DashboardProvider} from "@/admin/dashboard/components/dashboard-provider";
-import {EmployeeProvider} from "@/employee/employee-components/employee-provider";
+import { DashboardProvider } from "@/admin/dashboard/components/dashboard-provider";
+import { EmployeeProvider } from "@/employee/employee-components/employee-provider";
 import dynamic from "next/dynamic";
+import { redirect } from "next/navigation";
 
 // temp TODO -- do this properly with components rather than pages..
-const DashboardPage = dynamic(() => import('@/admin/dashboard/page'));
-const EmployeePage = dynamic(() => import('@/employee/page'));
+const DashboardPage = dynamic(() => import("@/admin/dashboard/page"));
+const EmployeePage = dynamic(() => import("@/employee/page"));
 
 /**
  * The root page of the app. Loads employee/admin specific dashboard.
@@ -14,36 +15,18 @@ const EmployeePage = dynamic(() => import('@/employee/page'));
  * @route `/`
  */
 export default function RootPage() {
-  const {role} = useAuth()
+  const { role } = useAuth();
 
   if (!role) {
-    return (
-        <></> // todo
-    )
+    // loading / corrupt user
+    return <></>;
   }
 
-  if (role?.EmployeePermission) {
-    return (
-        <DashboardProvider>
-          <DashboardPage/>
-        </DashboardProvider>
-    )
+  if (role.EmployeePermission) {
+    redirect(`/admin`);
   } else {
-    // this causes a refresh on data when leaving the main page..
-    return (
-        <EmployeeProvider>
-          <EmployeePage/>
-        </EmployeeProvider>
-    )
+    redirect(`/employee`);
   }
-  // return (
-  //     <>
-  //         <div className="md:hidden">
-  //             Old Dashboard
-  //         </div>
-  //     </>
-  // )
 }
-
 
 //todo mobile responsiveness , data provider
