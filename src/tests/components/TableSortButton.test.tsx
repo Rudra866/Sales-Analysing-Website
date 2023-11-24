6735bdd1c8d84f4f0c70f663065e1b612db61e5d
rudrapatel <cvb652@usask.ca>
2023-11-24 00:06:02 -0600

    TableSortButton.test done

--- src/tests/components/TableSortButton.test.tsx
+++ src/tests/components/TableSortButton.test.tsx
@@ -1,3 +1,27 @@
-test("pass", () => {
-  // todo
-})
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import TableSortButton from "@/components/tables/table-sort-button";
+import { Column } from "@tanstack/react-table";
+import React from "react";
+
+describe("TableSortButton", () => {
+    const mockGetIsSorted = jest.fn();
+    const mockToggleSorting = jest.fn();
+    const mockColumn = {
+        id: "Test Column",
+        getIsSorted: mockGetIsSorted,
+        toggleSorting: mockToggleSorting,
+    } as Partial<Column<any>> as Column<any>;
+
+    test('renders the button with the correct text', () => {
+        render(<TableSortButton column={mockColumn} />);
+        expect(screen.getByRole("button", { name: /Test Column/i })).toBeInTheDocument();
+    });
+
+    test('clicking the button toggles sorting', () => {
+        mockGetIsSorted.mockReturnValueOnce("asc");
+        render(<TableSortButton column={mockColumn} />);
+        userEvent.click(screen.getByRole("button"));
+        expect(mockToggleSorting).toHaveBeenCalledWith("desc");
+    });
+});
